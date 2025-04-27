cb7609cbfef938a6a0457536f2d459592b63c2b4
ArjunMagar <arjunmagar6820@gmail.com>
2025-04-28 05:04:58 +0545

    multer update

--- src/middleware/multerConfig.js
+++ src/middleware/multerConfig.js
@@ -1,12 +1,22 @@
 const multer = require('multer')
+const path = require('path')
+const fs = require('fs')
+
+// Constructing the upload path
+const uploadPath = path.join(__dirname, '..', 'uploads');
+
+// Check/Ensuring if the 'uploads' directory/folder exists; if not, create it
+if (!fs.existsSync(uploadPath)) {
+    fs.mkdirSync(uploadPath, { recursive: true }); // This creates the folder if it doesn't exist
+}
 
 const storage = multer.diskStorage({
-    destination : function(req,file,cb){
-        cb(null,'./src/uploads')
-    }, 
-    filename : function(req,file,cb){
-        cb(null,Date.now() + "-" + file.originalname)
+    destination: function (req, file, cb) {
+        cb(null, uploadPath)    // Saving files to the 'uploads' directory
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + "-" + file.originalname) // Naming the file with a timestamp
     }
 })
 
-module.exports = {multer,storage}
+module.exports = { multer, storage }
