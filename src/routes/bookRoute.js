62d49e4807445a5e10a755933f69e1001abcbacf
ArjunMagar <arjunmagar6820@gmail.com>
2025-04-27 18:58:56 +0545

    add multer middleware for uploading file

--- src/routes/bookRoute.js
+++ src/routes/bookRoute.js
@@ -1,9 +1,9 @@
 const bookController = require("../controllers/bookController")
-
 const router = require("express").Router()
+const{multer,storage} = require('../middleware/multerConfig')
+const upload = multer({storage : storage})
 
-
-router.route('/book').post(bookController.addBook).get(bookController.fetchBooks)
+router.route('/book').post(upload.single('image'),bookController.addBook).get(bookController.fetchBooks)
 router.route('/book/:id').get(bookController.fetchBook).patch(bookController.updateBookById).delete(bookController.deleteBook)
 
 
